b8b10cb4a0eaba713ebaa5c51180f1d49008aad6
Victor Eduardo Mendoza-Lopez <victormendoza2008@outlook.com>
2025-02-13 13:32:53 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,7 +18,41 @@ class _MyWidgetState extends State<HomePage> {
           Icons.menu,
           color: Colors.grey[800],
         ),
-        actions: [Icon(Icons.person)],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          //1. Texto principal (Text)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                Text("Eat",
+                    style: TextStyle(
+                        // Tamaño de letra
+                        fontSize: 32,
+                        // Negritas
+                        fontWeight: FontWeight.bold,
+                        //Subrayado
+                        decoration: TextDecoration.underline))
+              ],
+            ),
+          )
+          //2. Pestañas (TabBar)
+          //3. Contenido de pestañas (TabBarView)
+          //4. Carrito (Cart)
+        ],
       ),
     );
   }
