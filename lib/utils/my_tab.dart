9da1f9dca0458ccdaf83e2cbe22ef21f08f63396
Victor Eduardo Mendoza-Lopez <victormendoza2008@outlook.com>
2025-02-17 12:41:21 -0600

    Starting TabBarView

---
+++ lib/utils/my_tab.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyTab extends StatelessWidget {
+// Ruta del archivo
+  final String iconPath;
+
+  const MyTab({super.key, required this.iconPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      height: 50,
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            color: Colors.grey[200], borderRadius: BorderRadius.circular(16)
+            // Borde redondeado
+            ),
+        child: Image.asset(
+          // Ruta del archivo
+          iconPath,
+          color: Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
